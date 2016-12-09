cb32c17b91615011e05d4f609741a744759c0f27
DevSec <arma.devsec@gmail.com>
2016-12-09 18:40:49 +0100

    bad doc

--- public/javascripts/movies.js
+++ public/javascripts/movies.js
@@ -1,6 +1,5 @@
-$('.modal-trigger').leanModal()
-$('.tooltipped').tooltip({delay: 500})
-$('.modal-movie').modal({
+$('.modal-trigger').leanModal({
     dismissible: false, // Modal can be dismissed by clicking outside of the modal
     complete: function() { alert('Closed'); } // Callback for Modal close
 })
+$('.tooltipped').tooltip({delay: 500})
