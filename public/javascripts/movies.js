94ab62746b478f346df1afe77784d17adedb4550
DevSec <arma.devsec@gmail.com>
2016-12-09 18:38:20 +0100

    modal moved

--- public/javascripts/movies.js
+++ public/javascripts/movies.js
@@ -1,2 +1,6 @@
 $('.modal-trigger').leanModal()
 $('.tooltipped').tooltip({delay: 500})
+$('.modal-movie').modal({
+    dismissible: false, // Modal can be dismissed by clicking outside of the modal
+    complete: function() { alert('Closed'); } // Callback for Modal close
+})
